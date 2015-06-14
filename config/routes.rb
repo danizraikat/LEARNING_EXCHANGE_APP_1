0684f1fb06d6f609b14b20922aef962be756d220
danizraikat <danizraikat@gmail.com>
2015-06-14 11:59:44 +0100

    added username and date to notes index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  
-  root "home#index"  
+  root "notes#index"  
   resources :comments
   resources :tags
   resources :notes
   devise_for :users   
-  resources :users, only: [:index, :show]    
-   
+  resources :users, only: [:index, :show]       
 end
+
+
 
 # The priority is based upon order of creation: first created -> highest priority.
 # See how all your routes lay out with "rake routes".
