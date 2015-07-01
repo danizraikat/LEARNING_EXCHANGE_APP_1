637b557fe6d69547d64fda70cf0aca681844e4e8
danizraikat <danizraikat@gmail.com>
2015-07-01 14:58:00 +0100

    adding associations

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,6 +3,7 @@ class Note < ActiveRecord::Base
   belongs_to :user
   has_many :comments 
   has_and_belongs_to_many :tags
+  has_and_belongs_to_many :favorites
   # belongs_to :favorites
 
   #VALIDATIONS
